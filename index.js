fbc04dce7fb3d6bf78ca9a602b3292e15faa678d
Patryk <patrykmigala0@gmail.com>
2024-03-12 22:31:44 +0100

    add animation of o uniwersum, add list in header, add footer

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+window.addEventListener('scroll', reveal);
 
+function reveal() {
+    let reveals = document.querySelectorAll('.info');
+
+    for (let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealtop = reveals[i].getBoundingClientRect().top;
+        let revealpoint = 120;
+
+        if (revealtop < windowHeight - revealpoint) {
+            reveals[i].classList.add('info-active')
+        }
+    }
+}
 
 
