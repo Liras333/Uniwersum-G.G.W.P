64b0e139c2eaae98ff106eab673934f7e2e162d1
Patryk <patrykmigala0@gmail.com>
2024-03-17 22:54:44 +0100

    add back to top for PC

--- wyszukiwarka.js
+++ wyszukiwarka.js
@@ -50,3 +50,20 @@ function sortuj() {
     }
 }
 
+document.addEventListener('scroll', scrolls);
+
+function scrolls() {
+
+    if (scrollY >= 400) {
+        if (innerWidth <= 1000) {
+            document.querySelector('.back-to-top').style.display = "none"
+        } else {
+            document.querySelector('.back-to-top').style.display = "block"
+        }
+
+    }
+    else {
+        document.querySelector('.back-to-top').style.display = "none"
+    }
+}
+
