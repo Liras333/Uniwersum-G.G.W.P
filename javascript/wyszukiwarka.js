becce17ed764cd20c1608277264fedfc7d716df8
Patryk <patrykmigala0@gmail.com>
2024-03-28 15:22:19 +0100

    add tags for characters

--- javascript/wyszukiwarka.js
+++ javascript/wyszukiwarka.js
@@ -5,19 +5,29 @@ function search() {
     const searchText = input.value.toLowerCase();
     const postacie = document.querySelectorAll('.link-postac');
     postacie.forEach(postac => {
+
+        const tagi = postac.querySelectorAll(".tagi .tag");
         const p = postac.querySelector('.opis-postaci .nazwa');
+        const headerText = p.textContent.toLowerCase();
 
-        if (p) {
-            const headerText = p.textContent.toLowerCase();
 
-            if (headerText.includes(searchText)) {
-                postac.style.display = "block"
-            } else {
-                postac.style.display = "none";
-            }
+        if (headerText.includes(searchText)) {
+            postac.style.display = "block";
+        } else {
+            postac.style.display = "none";
         }
+        
+        tagi.forEach(tag => {
+            const tagE = tag.textContent.toLowerCase();
+            if (tagE.includes(searchText)) {
+                postac.style.display = "block";
+            }
+        })
+
+
     })
 }
+
 
 //sortowanie
 
