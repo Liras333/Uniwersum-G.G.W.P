958a322a0f8f5d93c47bfd1571f95889951ca715
Patryk <patrykmigala0@gmail.com>
2024-07-02 22:04:42 +0200

    add popup for swiaty

--- javascript/swiaty.js
+++ javascript/swiaty.js
@@ -1,9 +1,36 @@
-const to_left = document.querySelector('.to-left');
-const to_right = document.querySelector('.to-right');
-
 const img1 = document.querySelector('.img1');
 const img2 = document.querySelector('.img2');
+const popup = document.querySelector(".popup")
+const close_popup = document.querySelector(".close_popup")
+const obraz_popup = document.querySelector('.obraz_popup');
 
+console.log(obraz_popup)
+
+img1.addEventListener("click", () => {
+    popup.style.display = "flex";
+    obraz_popup.src = img1.src;
+})
+
+if (img2 == null) {
+    console.log("a")
+} else {
+    img2.addEventListener("click", () => {
+        popup.style.display = "flex";
+        obraz_popup.src = img2.src;
+    })
+}
+
+
+
+close_popup.addEventListener("click", () => {
+    popup.style.display = "none";
+})
+
+
+
+
+const to_left = document.querySelector('.to-left');
+const to_right = document.querySelector('.to-right');
 let images = document.querySelectorAll('.image-slider');
 
 to_left.addEventListener('click', () => {
@@ -31,3 +58,7 @@ to_right.addEventListener('click', () => {
     })
 
 })
+
+
+
+
