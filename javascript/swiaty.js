25acdf9c79c336275aaee028a0d481139af92a58
Patryk <patrykmigala0@gmail.com>
2024-07-03 18:18:21 +0200

    done swiaty.html

--- javascript/swiaty.js
+++ javascript/swiaty.js
@@ -1,10 +1,10 @@
+// POPUP
+
 const img1 = document.querySelector('.img1');
 const img2 = document.querySelector('.img2');
 const popup = document.querySelector(".popup")
 const close_popup = document.querySelector(".close_popup")
 const obraz_popup = document.querySelector('.obraz_popup');
-
-console.log(obraz_popup)
 
 img1.addEventListener("click", () => {
     popup.style.display = "flex";
@@ -12,7 +12,7 @@ img1.addEventListener("click", () => {
 })
 
 if (img2 == null) {
-    console.log("a")
+    
 } else {
     img2.addEventListener("click", () => {
         popup.style.display = "flex";
@@ -20,45 +20,41 @@ if (img2 == null) {
     })
 }
 
+// STRZAŁKI LEWO / PRAWO
 
+
+function to_left(){
+let images = document.querySelectorAll('.image-slider');
+
+    images.forEach(obraz => {
+        if (obraz.classList.contains('img1')) {
+            obraz.classList.remove('img1');
+            obraz.classList.add('img2');
+        } else if (obraz.classList.contains('img2')) {
+            obraz.classList.remove('img2');
+            obraz.classList.add('img1');
+        }
+    })
+}
+
+function to_right(){
+let images = document.querySelectorAll('.image-slider');
+
+    images.forEach(obraz => {
+        if (obraz.classList.contains('img1')) {
+            obraz.classList.remove('img1');
+            obraz.classList.add('img2');
+        } else if (obraz.classList.contains('img2')) {
+            obraz.classList.remove('img2');
+            obraz.classList.add('img1');
+        }
+    })
+}
+
+
+// CLOSE PUPUP
 
 close_popup.addEventListener("click", () => {
     popup.style.display = "none";
 })
-
-
-
-
-const to_left = document.querySelector('.to-left');
-const to_right = document.querySelector('.to-right');
-let images = document.querySelectorAll('.image-slider');
-
-to_left.addEventListener('click', () => {
-    images.forEach(obraz => {
-        if (obraz.classList.contains('img1')) {
-            obraz.classList.remove('img1');
-            obraz.classList.add('img2');
-        } else if (obraz.classList.contains('img2')) {
-            obraz.classList.remove('img2');
-            obraz.classList.add('img1');
-        }
-    })
-
-})
-
-to_right.addEventListener('click', () => {
-    images.forEach(obraz => {
-        if (obraz.classList.contains('img1')) {
-            obraz.classList.remove('img1');
-            obraz.classList.add('img2');
-        } else if (obraz.classList.contains('img2')) {
-            obraz.classList.remove('img2');
-            obraz.classList.add('img1');
-        }
-    })
-
-})
-
-
-
 
